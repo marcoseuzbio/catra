e147f9162c613dd2f1fde37f8b43f5729bbacf04
Marcos Antonio Euzébio de Oliveira <maoliveira@artrj.local>
2019-02-25 16:15:27 -0300

    novas funções e uma adaptação a função reajvar

--- README.md
+++ README.md
@@ -13,4 +13,9 @@ enum(x2)
 enum(x3)
 a função retorna T
 ## função reajvar
-A função tem por objetivo apenas utilizar as linhas que contenham algum valor númerico, e assim descartar as que não contenham valor númerico, ela também transforma as linhas excluidas em titulo
+A função tem por objetivo apenas utilizar as linhas que contenham algum valor númerico, e assim descartar as que não contenham valor númerico, ela também transforma as linhas excluidas em titulo. A função retorna uma lista se for informado um segundo valor como True
+## Função opera
+A função tem como objetivo ler um arquivo excel, verificar quais variáveis contém datas, e assim fazer um banco de dados com datas.
+Esta função deve ser utilizada com cuidado, visto que ela depende de variáveis dentro do primeiro excel.
+## FUnção opera1
+Esta função tem como objetivo utilizar a função opera porém esperando que tenha sido dito que existe data, hora, ou interval
--- funções para ajeitar o banco de dados.R
+++ funções para ajeitar o banco de dados.R
@@ -1,10 +1,14 @@
+# verificando se os pacotes que quero estão instalados
+list.of.packages <- c("readxl")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 #função para verificar se é número a string
 # x é numero
 enum<-function(x){
   grepl("^\\d+(\\.|\\,)\\d+$",x)
 }
 #reajusta o data.frame, x é data frame
-reajvar<-function(x){
+reajvar<-function(x,lista=F){
   if (is.data.frame(x)==F){
     x=as.data.frame(x)
   }
@@ -45,7 +49,103 @@ for (i in 1:nrow(xt))
 else
           names(y)[j]<- paste(names(y)[j],x[i,j],sep = '-')
         }
-  
-return(y)
+  if (lista==F) return(y)
+else
+return(list(dataf=y,valor=n))
 }
 
+# Mesma função de cima, porém transforma as datas
+# options(chron.origin = c(month=12, day=30, year=1899))
+# catra1<-function(x,lista1=NULL,lista2=NULL){
+#   # x a lista, data frame, etc
+#   # lista1 é a lista de palavras que identificam datas
+#   # primeiro ajeito o banco, ainda tendo os números
+#   x1<- reajvar(x)
+#   x11<-as.data.frame(x1)
+#   # agora verifico se tenho alguma palavra a verificar
+#   if (!is.null(lista1)){
+#     # quantas palavras vou verificar
+#     n<-length(lista1)
+#     for (i in 1:n){
+#       # para cada palavra, verifico quais colunas vou verificar
+#       ver<-grepl(lista1[i],names(x1))
+#       # tomo a quantidade de colunas
+#       n1<-length(names(x1))
+#       for (j in 1:n1){
+#         # se a minha coluna contém a palavra
+#         if (ver[j]==T){
+#           # percorro linha por linha modificando o format0
+#           n2=length(x1[1,])
+#           for (k in 1:n2){
+#             x11[k,j] <-as.Date(as.numeric(x1[k,j]), origin = "1899-12-30")
+#           }
+#         }
+#       }
+#     }
+#   }
+#   # agora verifico se tenho alguma palavra a verificar
+#   if (!is.null(lista2))
+#   {
+#     # quantas palavras vou verificar para tempo
+#     n<-length(lista2)
+#     for (i in 1:n){
+#       # para cada palavra, verifico quais colunas vou verificar
+#       ver<-grepl(lista2[i],names(x1))
+#       # tomo a quantidade de colunas
+#       n1<-length(names(x1))
+#       for (j in 1:n1){
+#         # se a minha coluna contém a palavra
+#         if (ver[j]==T){
+#           # percorro linha por linha modificando o format0
+#           n2=length(x1[1,])
+#           for (k in 1:n2){
+#             x11[k,j] <-chron::as.chron(as.numeric(x1[k,j]))
+#           }
+#         }
+#       }
+#     }
+#   }
+#   return(x11)
+# }
+#ler o banco já ajeitando os dados
+opera<-function(x,lista1=NULL){
+  # primeiro leio o arquivo
+  x2<-readxl::read_excel(x)
+  #aqui reajusto o arquivo
+  x1x<-reajvar(x2,lista = T)
+  # separo o arquivo entre os valores e as linhas que não quero
+  x1<-as.data.frame(x1x$dataf)
+  nn<-as.vector(x1x$valor)
+  # pego os nomes das linhas
+  name<-names(x1)
+  # digo que nenhuma coluna corresponde a data
+  ver1<-rep(F,ncol(x1))
+  if(!is.null(lista1)){
+    # pego a quantidade de valores que vou procurar
+    n=length(lista1)
+    for (i in 1:n){
+      # procuro quais colunas vou verificar
+      ver<-grepl(lista1[i],names(x1))
+      ver1[which(ver==T)]=T
+    }
+    # assumo que todos serão lidos como padrão, ou seja, texto
+  ver2=rep("text",ncol(x1))
+  # quais eu disse que verei, mudo para data
+  ver2[which(ver1==T)]="date"
+  # leio com o novo formato
+  x1<-readxl::read_excel(x,col_types = ver2)
+  }
+  # quais eu não queroeu excluo
+  x2<-x1[-which(nn==T),]
+  #transformo em data frame
+  x2<-as.data.frame(x2)
+  # atribuo os nomes
+  names(x2)<-name
+  return(x2)
+}
+opera1<-function(x){
+  #essa função usa a função opera porém com uma lista1 padrão
+  padronizado=c("DATA","HORA","INTERVAL")
+  x1<-opera(x,padronizado)
+  return(x1)
+}
